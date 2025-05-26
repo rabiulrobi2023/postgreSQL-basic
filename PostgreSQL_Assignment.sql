1d22ab3a36d1aebfb623c53023ac5ef54da7a6e5
Rabiul Islam <rabiul.robi2023@gmail.com>
2025-05-26 08:35:34 +0600

    Code is cleaned

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,41 +1,31 @@
 -- Active: 1747409864817@@127.0.0.1@5432@conservation_db@public
 CREATE TABLE rangers (
     ranger_id SERIAL PRIMARY KEY, 
-    name VARCHAR(50) ,
+    name VARCHAR(50) NOT NULL ,
     region VARCHAR(50) 
 );
-
-DROP TABLE rangers;
 
 CREATE TABLE species (
     species_id SERIAL PRIMARY KEY,
     common_name VARCHAR(50) NOT NULL,
     scientific_name VARCHAR(50) NOT NULL,
-    discovery_date DATE ,
-    conservation_status VARCHAR(50) 
+    discovery_date DATE NOT NULL ,
+    conservation_status VARCHAR(50) NOT NULL
 );
-
-DROP TABLE species;
 
 CREATE TABLE sightings(
-    sighting_id SERIAL,
-    ranger_id  INT REFERENCES "rangers"(ranger_id),
-    species_id INT REFERENCES "species"(species_id),
-    sighting_time TIMESTAMP,
-    location VARCHAR(20) ,
+    sighting_id SERIAL PRIMARY KEY,
+    ranger_id  INT REFERENCES "rangers"(ranger_id) NOT NULL,
+    species_id INT REFERENCES "species"(species_id) NOT NULL,
+    sighting_time TIMESTAMP NOT NULL,
+    location VARCHAR(20) NOT NULL ,
     notes VARCHAR (255)
 );
-
-DROP TABLE sightings;
-
-
 
 INSERT INTO rangers (name, region) VALUES 
 ('Alice Green ','Northern Hills'),
 ('Bob White','River Delta '),
 ('Carol King','Mountain Range');
-
-select * from rangers;
 
 INSERT INTO species (common_name, scientific_name, discovery_date, conservation_status) VALUES
 ('Snow Leopard', 'Panthera uncia', '1775-01-01', 'Endangered'),
@@ -43,25 +33,20 @@ INSERT INTO species (common_name, scientific_name, discovery_date, conservation_
 ('Red Panda', 'Ailurus fulgens', '1825-01-01', 'Vulnerable'),
 ('Asiatic Elephant', 'Elephas maximus indicus', '1758-01-01', 'Endangered');
 
-SELECT * FROM species;
-
 INSERT INTO sightings (species_id, ranger_id, location, sighting_time, notes) VALUES
 (1, 1, 'Peak Ridge',        '2024-05-10 07:45:00', 'Camera trap image captured'),
 (2, 2, 'Bankwood Area',     '2024-05-12 16:20:00', 'Juvenile seen'),
 (3, 3, 'Bamboo Grove East', '2024-05-15 09:10:00', 'Feeding observed'),
 (1, 2, 'Snowfall Pass',     '2024-05-18 18:30:00', NULL);
 
-SELECT * FROM sightings;
-
-ALTER TABLE sightings
-    ALTER COLUMN sighting_time TYPE TIMESTAMP;
 
 
 -------------------------Problem-1-------------------------
 INSERT INTO rangers (name, region) VALUES ('Derek Fox', 'Coastal Plains');
 
+
 -------------------------Problem-2-------------------------
-SELECT count(SELECT  ranger_id FROM sightings GROUP BY ranger_id) as unique_species_count FROM sightings;
+SELECT count(DISTINCT ranger_id) as unique_species_count FROM sightings;
 
 
 -------------------------Problem-3-------------------------
@@ -90,11 +75,11 @@ LIMIT 2;
 -------------------------Problem-7--------------------------
 UPDATE  species
 SET conservation_status =  'Historic'
-WHERE EXTRACT(YEAR FROM discovery_date )<=1800;
+WHERE EXTRACT(YEAR FROM discovery_date )<1800;
 
 
 -------------------------Problem-8--------------------------
-SELECT sighting_id,sighting_time, 
+SELECT sighting_id, 
     CASE 
         WHEN  sighting_time::TIME<'12:00' THEN 'Morning'
         WHEN sighting_time::TIME<='17:00' THEN 'Afternoon'
